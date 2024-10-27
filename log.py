8cfc314bfe250b04b2a55e834d736721a4f626f3
antiman9071 <anthonyjedi9071@gmail.com>
2024-10-27 02:47:51 -0400

    added the rest of the possible flask code

--- log.py
+++ log.py
@@ -1,42 +1,36 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///log.db"
-db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///log.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db  = SQLAlchemy(app)
 
-class log(db.Model):
-    __tablename__ = 'log'
+class logEntry(FlaskForm):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200), default=f'Bandit{id}')
-    password = db.Column(db.String(200), nullable = False, default='test')
-    method = db.Column(db.String(200), nullable = False, default='test')
-    def __repr__(self):
-        return '<Challenege %r>' % self.id
+    name = db.Column(db.String(255), nullable=False)
+    passwd = db.Column(db.String(255), nullable = False)
+    method = db.Column(db.String(255), nullable = False)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
-        log_id = request.form['id']
-        log_passwd = request.form['password']
-        log_method = request.form['method']
-        new_id = log(id=log_id)
-        new_passwd= log(password=log_passwd)
-        new_method = log(method=log_method)
+        name = request.form["username"]
+        passwd = request.form["password"]
+        method = request.form["method"]
+        newLogEntry = logEntry(name=name, passwd=passwd, method=method)
+        db.session.add(newLogEntry)
+        db.session.commit()
+        return redirect(url_for("index"))
+    entries = Entry.query.all()
 
-        try:
-            db.session.add(new_id)
-            db.session.add(new_passwd)
-            db.session.add(new_method)
-            db.session.commit()
-            return redirect('/')
-        except Exception as e:
-            return f"There was an error\n{e}"
-        
+    return render_template("index.html", entries=entries)
+
+if __name__ == "__main__":
+    if os.path.exists(./log.db):
+        pass
     else:
-        logs = log.query.order_by(log.id).all()
-        return render_template('index.html', logs=logs)
-
-app.run(host="0.0.0.0", port=80)
+        db.create_all()
+        
+    app.run(host="0.0.0.0", port=80)
